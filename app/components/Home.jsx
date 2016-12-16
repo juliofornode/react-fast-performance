67344805499de85e5aad826464505eab8f2ef974
Julio <juliofornode@gmail.com>
2016-12-16 14:59:01 -0800

    added components to display custom input field, input label and form

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -7,6 +7,9 @@ import GitHub from './GitHub';
 import Workflow from './Workflow';
 import ButtonDemo from './ButtonDemo';
 import Button from './Button';
+import Input from './Input';
+import InputLabel from './InputLabel';
+import InputField from './InputField';
 
 export default class Home extends Component {
   render() {
@@ -137,7 +140,7 @@ export default class Home extends Component {
         </section>
 
         <section className="stripe">
-          
+
           <Card className="col--one-third text--center back--white">
             <p>Click does not do much...</p>
             <Button label="Default" color="default" />
@@ -164,6 +167,43 @@ export default class Home extends Component {
           </Card>
 
         </section>
+
+        <section className="stripe back--default">
+                  <Card plain className="col--one-third text--center">
+                    <h1>Responsive Forms</h1>
+                    <p className="subtext">
+                      Using variations of input controls and buttons.
+                    </p>
+                  </Card>
+                  <Card className="col--quarter back--white">
+                    <Input>
+                      <InputLabel label="Name" icon="user" />
+                      <InputField placeholder="Placeholder for name" />
+                    </Input>
+                    <Input>
+                      <InputLabel label="Street"/>
+                      <InputField placeholder="Enter street address" />
+                    </Input>
+                    <Input>
+                      <InputField placeholder="Just a field" />
+                    </Input>
+                  </Card>
+                  <Card className="back--white">
+                    <Input>
+                      <InputField />
+                      <Button color="success" icon="search" />
+                    </Input>
+                    <Input>
+                      <Button color="primary" icon="cloud" label="Connect" />
+                      <InputField placeholder="Enter server address" />
+                    </Input>
+                    <Input>
+                      <InputLabel icon="envelope" />
+                      <InputField placeholder="Send a note" />
+                      <Button color="warning" label="Send" />
+                    </Input>
+                  </Card>
+                </section>
 
         <section className="stripe--slim back--gray">
 
